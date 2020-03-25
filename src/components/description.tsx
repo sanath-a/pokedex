d49f473ffdc6b8742a5dede9da2272439cbf2c92
Sanath Angalakudati <sangalakudati@gmail.com>
2020-03-24 19:54:43 -0700

    simplified description

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -8,57 +8,46 @@ type descProps = {
     games: string,
     abilities: string
 }
-type descriptionState = {
-    hasGames: boolean
-}
-export class Description extends React.Component<descProps, descriptionState> {
-    constructor(props: descProps) {
-        super(props);
-        this.state = {
-            hasGames: this.props.games != null,
-        }
+
+export function Description(props: descProps) {
+    let games:JSX.Element;
+    console.log(props.games);
+    if(props.games) {
+        games = (
+            <p>
+                <span className= "desc_head">Games: </span>
+                {props.games}
+            </p>
+        )
+    } else {
+        games = (
+            <p>
+                <span className= "desc_head">Games: </span>
+                {"No information available"}
+            </p>
+        )
     }
 
-    render() {
-        let games:JSX.Element;
-        console.log(this.props.games);
-        if(this.props.games) {
-            games = (
-                <p>
-                    <span className= "desc_head">Games: </span>
-                    {this.props.games}
-                </p>
-            )
-        } else {
-            games = (
-                <p>
-                    <span className= "desc_head">Games: </span>
-                    {"No information available"}
-                </p>
-            )
-        }
-        console.log(games);
-        return (
-            <div className="description">
-                <h3>
-                    <span className= "desc_head">  Name: </span>
-                    {this.props.name}
-                </h3>
-                <h4> <span className = "desc_head"> Type: </span> {this.props.type}</h4>
-                <h4>
-                    <span className= "desc_head">  Height: </span> {
-                    Math.round(this.props.height * 10).toString() + 'cm'},
-                    <span className= "desc_head"> Weight: </span>
-                    {Math.round(this.props.weight * 0.1) + 'kg'}
-                </h4>
-                <p>
-                    <span className = "desc_head"> Abilities: </span>
-                    {this.props.abilities}
-                    <br/>
-                </p>
-                {games}
-            </div>
-        );
+    return (
+        <div className="description">
+            <h3>
+                <span className= "desc_head">  Name: </span>
+                {props.name}
+            </h3>
+            <h4> <span className = "desc_head"> Type: </span> {props.type}</h4>
+            <h4>
+                <span className= "desc_head">  Height: </span> {
+                Math.round(props.height * 10).toString() + 'cm'},
+                <span className= "desc_head"> Weight: </span>
+                {Math.round(props.weight * 0.1) + 'kg'}
+            </h4>
+            <p>
+                <span className = "desc_head"> Abilities: </span>
+                {props.abilities}
+                <br/>
+            </p>
+            {games}
+        </div>
+    );
 
-    }
 }
